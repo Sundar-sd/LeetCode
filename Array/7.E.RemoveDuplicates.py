e0d2ba15660c1666ab70c456530aa4b2bdd4e724
Sundar-sd <sundar00500@gmail.com>
2025-12-29 19:35:34 +0530

    Completed!

---
+++ Array/7.E.RemoveDuplicates.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def removeduplicates(self , nums:List[int]):
+
+        write = 1
+
+        for i in range (1,len(nums)):
+            if nums[i]!= nums[i-1]:
+                nums[write] = nums[i]
+                write+=1
+
+        return write
+
+
+nums =[1,1,7,7,8,9,9]
+sol = Solution()
+result = sol.removeduplicates(nums)
+print (result)
+            
