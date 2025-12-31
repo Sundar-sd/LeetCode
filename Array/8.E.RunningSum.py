f69d8e39da7b6cce8bb979c8b1fcd77743f582cf
Sundar-sd <sundar00500@gmail.com>
2025-12-31 21:50:34 +0530

    Small Step Yet a Step!

---
+++ Array/8.E.RunningSum.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range (1,len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+
+nums = [1,2,3,4]
+sol = Solution()
+result = sol.runningSum(nums)
+print (result)
