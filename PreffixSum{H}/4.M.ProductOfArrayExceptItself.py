976259d5309d91f0abccb3661bf138467e83dff5
Sundar-sd <sundar00500@gmail.com>
2026-01-10 17:37:42 +0530

    m

---
+++ PreffixSum{H}/4.M.ProductOfArrayExceptItself.py
@@ -0,0 +1,23 @@
+from typing import List
+class Solution:
+    def ex(self , nums:List[int]):
+
+        n = len(nums)
+        ans = [1]*n
+
+        prefix = 1
+        for i in range(n):
+            ans[i] = prefix
+            prefix *= nums[i]
+
+        sufix = 1
+        for i in range(n-1 , -1 ,-1):
+            ans[i] *= sufix
+            sufix *= nums[i]
+        
+        return ans 
+    
+nums = [1,2,3,4]
+sol = Solution()
+result = sol.ex(nums)
+print(result)
